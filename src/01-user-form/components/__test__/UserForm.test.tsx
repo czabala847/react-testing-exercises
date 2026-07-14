8f83abbd0efd703bdf728baee75cf1c326e199fe
Carlos Zabala <czabala847@gmail.com>
2026-07-14 17:26:12 -0500

    Add coverage to .gitignore and update UserForm tests with user-event interactions

--- src/01-user-form/components/__test__/UserForm.test.tsx
+++ src/01-user-form/components/__test__/UserForm.test.tsx
@@ -1,8 +1,10 @@
+ 
 import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
 import { UserForm } from "../UserForm"
 
 describe("UserForm", () => {
-  test("it shows two inputs and a button", () => {
+  it("it shows two inputs and a button", () => {
     //render the component
     render(<UserForm onUserAdd={() => {}} />);
 
@@ -13,5 +15,28 @@ describe("UserForm", () => {
     //assertion
     expect(inputs).toHaveLength(2);
     expect(button).toBeInTheDocument();
+  });
+
+  it("it calls onUserAdd when the form is submitted", async () => {
+    const user = userEvent.setup()
+
+    const mock = jest.fn();
+    render(<UserForm onUserAdd={mock} />);
+
+    // const [name, email] = screen.getAllByRole("textbox");
+    const name = screen.getByRole("textbox", { name: /name/i });
+    const email = screen.getByRole("textbox", { name: /email/i });
+    const button = screen.getByRole("button");
+
+    //simulate typing in the inputs
+    await user.type(name, "test user");
+    await user.type(email, "Hd0e2@example.com");
+
+    //simulate clicking the button
+    await user.click(button);
+
+    //assertion
+    expect(mock).toHaveBeenCalledTimes(1);
+    expect(mock).toHaveBeenCalledWith({ name: "test user", email: "Hd0e2@example.com" });
   });
 });
