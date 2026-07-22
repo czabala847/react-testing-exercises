e47563be2c87f7dc9bf6b212e5182e0a2c709bb1
Carlos Zabala <czabala847@gmail.com>
2026-07-21 21:33:13 -0500

    Refactor RepositoriesSummary test to validate all repository information

--- src/02-codesplain-starter/components/repositories/__tests__/RepositoriesSummary.test.tsx
+++ src/02-codesplain-starter/components/repositories/__tests__/RepositoriesSummary.test.tsx
@@ -2,10 +2,26 @@ import { render, screen } from "@testing-library/react"
 import RepositoriesSummary from "../RepositoriesSummary"
 
 describe("RepositoriesSummary", () => {
-  it("displays the primary language of the repository", () => {
-    const repository = { stargazers_count: 10, open_issues: 5, forks: 3, language: "TypeScript" };
+  it("displays information about the repository", () => {
+    const repository = {
+      stargazers_count: 10,
+      open_issues: 5,
+      forks: 3,
+      language: "TypeScript",
+    };
     render(<RepositoriesSummary repository={repository} />);
 
-    expect(screen.getByText("TypeScript")).toBeInTheDocument();
+    for (const key in repository) {
+      const value = repository[key as keyof typeof repository];
+      const element = screen.getByText(new RegExp(value.toString()));
+      expect(element).toBeInTheDocument();
+    }
   });
+
+  //   it("displays the primary language of the repository", () => {
+  //     const repository = { stargazers_count: 10, open_issues: 5, forks: 3, language: "TypeScript" };
+  //     render(<RepositoriesSummary repository={repository} />);
+
+  //     expect(screen.getByText("TypeScript")).toBeInTheDocument();
+  //   });
 });
